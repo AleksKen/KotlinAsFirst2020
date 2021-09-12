ff2419236cb9114fb1cb240cd2b9b6c1ec85f2a2
mariakonasova <aleks.ken2017@mail.ru>
2021-09-12 23:27:30 +0300

    второй урок, первая часть

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -200,10 +200,10 @@ fun triangleKind(a: Double, b: Double, c: Double): Int  {
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
     var ans = -1
-    var s1  = Array(10000, {i -> 0})
+    var s1  = Array(100000, {i -> 0})
     for (i in a..b)
         s1[i] = 1
-    var s2  = Array(10000, {i -> 0})
+    var s2  = Array(100000, {i -> 0})
     for (i in c..d)
         s2[i] = 1
     for (i in 0..s1.size)
