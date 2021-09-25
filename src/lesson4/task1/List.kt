eea5d751b6345ea9464dab47f72aa9c287672ef2
mariakonasova <aleks.ken2017@mail.ru>
2021-09-25 08:24:15 +0300

    урок 4 (две задачи)

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -370,8 +370,8 @@ fun russian(n: Int): String {
             when (thou) {
                 1 -> ans += a[1000]
                 2 -> ans += a[2000]
-                in 3..4 -> ans += a[thou] + " " + a[3000]
-                in 5..9 -> ans += a[thou] + " " + a[5000]
+                in 3..4 -> ans += a[thou] + " " + a[3000]+" "
+                in 5..9 -> ans += a[thou] + " " + a[5000]+" "
             }
         else {
             val hun = thou / 100 * 100
