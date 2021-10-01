2b23c93b996ef6eacffab65e3ebe27f09c3d776b
mariakonasova <aleks.ken2017@mail.ru>
2021-10-01 08:51:28 +0300

    урок 5 (бинарный поиск)

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -280,14 +280,17 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
-    var ans:Pair<Int, Int> = Pair(-1, -1)
-    for (i in 0..list.lastIndex-1)
-        for (j in i+1..list.lastIndex)
-            if (list[i]+list[j]==number){
-                ans=Pair(i, j)
-                break
-            }
-    return ans}
+    var ans: Pair<Int, Int> = Pair(-1, -1)
+    for (i in 0..list.lastIndex) {
+        val sample = number - list[i]
+        val index = list.binarySearch(sample)
+        if ((index >= 0) && (index!=i))
+            ans = Pair(i, index)
+        break
+    }
+    return ans
+}
+
 
 /**
  * Очень сложная (8 баллов)
