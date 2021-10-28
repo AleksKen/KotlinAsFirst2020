09dc7dc98412e50bcd87a074be3bdf2998b31b92
mariakonasova <aleks.ken2017@mail.ru>
2021-10-28 19:31:16 +0300

    урок 7 номер 1

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -63,8 +63,17 @@ fun alignFile(inputName: String, lineLength: Int, outputName: String) {
  * Подчёркивание в середине и/или в конце строк значения не имеет.
  */
 fun deleteMarked(inputName: String, outputName: String) {
-    TODO()
-}
+    var writer = File(outputName).bufferedWriter()
+    for (line in File(inputName).readLines()){
+        if (line.isEmpty())
+            writer.newLine()
+        else
+            if (line[0]!='_'){
+                writer.write(line)
+            writer.newLine()}}
+    writer.close()
+    }
+
 
 /**
  * Средняя (14 баллов)
