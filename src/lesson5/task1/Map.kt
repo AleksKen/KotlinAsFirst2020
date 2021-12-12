56d0b1939cac871f345f025d120529d8f62c87c1
mariakonasova <aleks.ken2017@mail.ru>
2021-12-12 10:57:33 +0300

    урок 7

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -316,33 +316,35 @@ fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
  */
 
 class Item(var name: String, var weight: Int, var price: Int)
-class BagPack(var iteams: String, var price: Int)
+class BagPack(var iteams: MutableSet<String>, var price: Int)
 
 fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<String> {
     var items = mutableListOf<Item>()
-    var ansGap = mutableSetOf<String>()
 
     var treasureList = treasures.toList()
     for (i in 0..treasureList.size - 1)
         items.add(Item(treasureList[i].first, treasureList[i].second.first, treasureList[i].second.second))
 
-    var bp: Array<Array<BagPack>> = Array(items.size + 1, { Array(capacity + 1, { BagPack("", 0) }) })
+    var bp: Array<Array<BagPack>> =
+        Array(items.size + 1, { Array(capacity + 1, { BagPack(mutableSetOf<String>(), 0) }) })
 
     for (i in 0..items.size)
         for (j in 0..capacity) {
             if ((i == 0) || (j == 0))
-                bp[i][j] = BagPack("", 0)
+                bp[i][j] = BagPack(mutableSetOf<String>(), 0)
             else if (i == 1) {
                 if (items[0].weight <= j)
-                    bp[1][j] = BagPack(items[0].name, items[0].price)
+                    bp[1][j] = BagPack(mutableSetOf<String>(items[0].name), items[0].price)
                 else
-                    bp[1][j] = BagPack("", 0)
+                    bp[1][j] = BagPack(mutableSetOf<String>(), 0)
             } else if (items[i - 1].weight > j) {
                 bp[i][j] = bp[i - 1][j]
             } else {
                 var prev = bp[i - 1][j]
+                var set: MutableSet<String> = bp[i - 1][j - items[i - 1].weight].iteams
+                set.add(items[i - 1].name)
                 var form = BagPack(
-                    bp[i - 1][j - items[i - 1].weight].iteams + ' ' + items[i - 1].name,
+                    set,
                     items[i - 1].price + bp[i - 1][j - items[i - 1].weight].price
                 )
                 if (prev.price > form.price)
@@ -351,16 +353,14 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
                     bp[i][j] = form
             }
         }
-
     var maxPrice = 0
-    var ansList = listOf<String>()
+    var index = 0
     for (i in 0..items.size)
         if (bp[i][capacity].price > maxPrice) {
             maxPrice = bp[i][capacity].price
-            ansList = (bp[i][capacity].iteams).split(" ")
-
+            index = i
         }
-    return ansList.toSet()
+    return bp[index][capacity].iteams
 }
 
 
